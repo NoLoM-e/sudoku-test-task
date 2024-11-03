85192b9503eaf3682bb253b1d26d2b2a00221a82
NoLoM <dyjey18@gmail.com>
2024-11-03 11:09:11 +0100

    initial commit

---
+++ src/test/java/com/example/sudokutesttask/SudokuTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sudokutesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
